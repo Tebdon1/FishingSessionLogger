87915ff179d44743e38083e3668691d8e6df82a6
Danhogg <daniel.hogg1@hotmail.co.uk>
2024-03-20 19:37:22 +0000

    Removed unused using statements

--- aspnet-core/src/SessionLogger.Domain/Sessions/Session.cs
+++ aspnet-core/src/SessionLogger.Domain/Sessions/Session.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace SessionLogger.Sessions;
