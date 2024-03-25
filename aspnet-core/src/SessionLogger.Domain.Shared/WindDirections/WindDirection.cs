8d9cf94e9ad68f0d52a8805dac3c3bcee2cd9319
Tebdon1 <134067312+Tebdon1@users.noreply.github.com>
2024-03-25 15:38:53 +0000

    WindDirection Enums

---
+++ aspnet-core/src/SessionLogger.Domain.Shared/WindDirections/WindDirection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SessionLogger.WindDirections;
+
+public enum SpeciesType
+{
+    North,
+    South,
+    East,
+    West,
+    NorthEast,
+    NorthWest,
+    SouthEast,
+    SouthWest
+}
