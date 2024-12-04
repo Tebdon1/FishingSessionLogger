849c13d1d7c36b01fa2e967017ca65287771805d
Danhogg <daniel.hogg1@hotmail.co.uk>
2024-12-04 00:39:23 +0000

    TopBar is now a component

--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { PageTopbarNavComponent } from '../components/page-topbar-nav/page-topbar-nav.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [SharedModule, HomeRoutingModule],
+  imports: [SharedModule, HomeRoutingModule, PageTopbarNavComponent],
 })
 export class HomeModule {}
